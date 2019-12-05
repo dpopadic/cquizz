ce5b3aa56ef602769c9759648d4d285a03a3d09a
dpopadic <dario.popadic@yahoo.com>
2019-12-05 16:28:26 +0000

    adding new sections

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ Useful url's
 - maths & programming problems platform
 > projecteuler.net/
 > https://www.janestreet.com/puzzles/
+> http://www.qbyte.org/puzzles/
 
 - quant finance topics
 > http://www.streetofwalls.com/finance-training-courses/
 
-
-
+- books
+> The Moscow Puzzles: 359 Mathematical Recreations
+> Heard on The Street: Quantitative Questions from Wall Street Job Interviews
 
 
--- intro.py
+++ intro.py
@@ -2,3 +2,14 @@
 import pandas as pd
 import numpy as np
 
+# how would you find a mean in an array? what's the complexity of the function?
+
+# write a program that outputs all prime numbers below N?
+
+# If I give you an array of integers, L, and another integer, s, can you write a program
+# to find out whether there exists a *pair* of integers in L which sum up to s. What's
+# the time complexity of this program? Can you find a faster version?
+
+
+
+
