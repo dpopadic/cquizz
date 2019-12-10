8064a3819872112190bda275489b71a65e876e99
dpopadic <dario.popadic@yahoo.com>
2019-12-10 18:18:45 +0000

    some additions

--- math/popular_algos.py
+++ math/popular_algos.py
@@ -122,10 +122,12 @@ def trans_unif_norm(x):
     return(z)
 
 
-
-
-
-
+# SORTING ALGO --------------------------------------------
+ax = [5, 3, 6, 8, 9, 12, 5, 1, 7]
+a = []
+k = 3
+for j in range(0, len(ax)-k+1):
+    a.append(np.sort(ax[j:(j+k)]))
 
 
 
