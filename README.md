3b78c34b7c07b684a1ea2a7b0a51176470a07f1f
dpopadic <dario.popadic@yahoo.com>
2019-12-26 14:06:30 +0000

    clear readme

--- README.md
+++ README.md
@@ -2,18 +2,20 @@
 Coding Quizzes
 
 > Largest prime factor of a number ```max_prime_factor```
-
+> Algorithm complexity ```algo_complexity```
+> Binary search```binary_search```
+> Recursive factorial algorithm```recursion_factorial```
+> Simple rolling sort ```roll_sort```
+> Newton's method square-root algorithm ```square_root_algo```
+> Uniform to normal distribution ```uniform_to_normal```
 
 
 Some useful url's
 -----------------
 
 - https://projecteuler.net/
-
 - https://www.janestreet.com/puzzles/
-
 - http://www.qbyte.org/puzzles/
-
 - https://www.geeksforgeeks.org/
 
 
--- ztl.py
+++ ztl.py
@@ -1,16 +1,10 @@
-
-import pandas as pd
-import numpy as np
-
-# how would you find a mean in an array? what's the complexity of the function?
-
-# write a program that outputs all prime numbers below N?
-
-# If I give you an array of integers, L, and another integer, s, can you write a program
+# list of upcoming algorithms..
+# - how would you find a mean in an array? what's the complexity of the function?
+# - write a program that outputs all prime numbers below N?
+# - If I give you an array of integers, L, and another integer, s, can you write a program
 # to find out whether there exists a *pair* of integers in L which sum up to s. What's
 # the time complexity of this program? Can you find a faster version?
-
-# write code that finds element in list that only appears once
+# - write code that finds element in list that only appears once
 
 # other:
 # https://github.com/zhiwehu/Python-programming-exercises/blob/master/100%2B%20Python%20challenging%20programming%20exercises.txt
